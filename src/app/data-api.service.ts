2387bbbdc8196b38a94118d45b4c04eef6629ce0
buckapi <buckapis@gmail.com>
2023-07-05 09:06:55 -0500

    new server api

--- src/app/data-api.service.ts
+++ src/app/data-api.service.ts
@@ -36,32 +36,32 @@ export class DataApiService {
 
   getTotalTransactions(userId: string){
   		let indice = userId;
-  		const url_api =  "https://db.bbevolutionbank.com:3025/api/transaction?filter[where][userId]=p"+indice;
+  		const url_api =  "https://db.buckapi.us:9999/api/transaction?filter[where][userId]=p"+indice;
 		return (this.transactions = this.http.get(url_api));
 	}
 	getInfo(){
-		const url_api=`https://db.bbevolutionbank.com:3025/api/infos/`;
+		const url_api=`https://db.buckapi.us:9999/api/infos/`;
 		this.info = this.http.get(url_api);
 		return (this.info);
 	}
 	getNewCreditcardsReturn(){
-		const url_api = 'https://db.bbevolutionbank.com:3025/api/creditcard?filter[where][status]=new';
+		const url_api = 'https://db.buckapi.us:9999/api/creditcard?filter[where][status]=new';
 		return (this.creditcards = this.http.get(url_api));
 	}
 	getActiveCreditcardsReturn(){
-		const url_api = 'https://db.bbevolutionbank.com:3025/api/creditcard?filter[where][status]=complete';
+		const url_api = 'https://db.buckapi.us:9999/api/creditcard?filter[where][status]=complete';
 		return (this.creditcards = this.http.get(url_api));
 	}	
 	getActiveAccountsReturn(){
-		const url_api = 'https://db.bbevolutionbank.com:3025/api/account?filter[where][status]=active';
+		const url_api = 'https://db.buckapi.us:9999/api/account?filter[where][status]=active';
 		return (this.accounts = this.http.get(url_api));
 	}	
 	getTransationsReturn(){
-		const url_api = 'https://db.bbevolutionbank.com:3025/api/transaction';
+		const url_api = 'https://db.buckapi.us:9999/api/transaction';
 		return (this.transactions = this.http.get(url_api));
 	}	
 	getNewAccountsReturn(){
-		const url_api = 'https://db.bbevolutionbank.com:3025/api/account?filter[where][status]=new';
+		const url_api = 'https://db.buckapi.us:9999/api/account?filter[where][status]=new';
 		return (this.accounts = this.http.get(url_api));
 	}	
 	sendMailNewCustomer(emailm){
@@ -71,75 +71,75 @@ export class DataApiService {
 		.pipe(map(data => data));
 	}
 	saveAccount(account :AccountInterface){
-		const url_api='https://db.bbevolutionbank.com:3025/api/account';
+		const url_api='https://db.buckapi.us:9999/api/account';
 		return this.http
 		.post<AccountInterface>(url_api, account)
 		.pipe(map(data => data));
 	}
 	newCreditcard(creditcard :CreditcardInterface){
-		const url_api='https://db.bbevolutionbank.com:3025/api/creditcard';
+		const url_api='https://db.buckapi.us:9999/api/creditcard';
 		return this.http
 		.post<CreditcardInterface>(url_api, creditcard)
 		.pipe(map(data => data));
 	}
 	updateAccount(account :AccountInterface, id: string){
-		const url_api=`https://db.bbevolutionbank.com:3025/api/account/${id}`;
+		const url_api=`https://db.buckapi.us:9999/api/account/${id}`;
 		return this.http
 		.put<AccountInterface>(url_api, account)
 		.pipe(map(data => data));
 	}
 	updateCreditcard(creditcard :CreditcardInterface, id: string){
-		const url_api=`https://db.bbevolutionbank.com:3025/api/creditcard/${id}`;
+		const url_api=`https://db.buckapi.us:9999/api/creditcard/${id}`;
 		return this.http
 		.put<CreditcardInterface>(url_api, creditcard)
 		.pipe(map(data => data));
 	}
 	updateTransaction(transaction :TransactionInterface, id: string){
-		const url_api=`https://db.bbevolutionbank.com:3025/api/transaction/${id}`;
+		const url_api=`https://db.buckapi.us:9999/api/transaction/${id}`;
 		return this.http
 		.put<TransactionInterface>(url_api, transaction)
 		.pipe(map(data => data));
 	}
 	getAccountByUserd2(userId: string){
 		let indice = userId;
-		const url_api =  "https://db.bbevolutionbank.com:3025/api/account?filter[where][userId]=p"+indice;
+		const url_api =  "https://db.buckapi.us:9999/api/account?filter[where][userId]=p"+indice;
 		this.account = this.http.get(url_api);
 		return (this.account);
 	}
 	getAccountByUserID(userId: string){
 		let indice = userId;
-		const url_api =  "https://db.bbevolutionbank.com:3025/api/account?filter[where][userId]="+indice;
+		const url_api =  "https://db.buckapi.us:9999/api/account?filter[where][userId]="+indice;
 		this.account = this.http.get(url_api);
 		return (this.account);
 	}
 	settingsUpdate(info :InfoInterface, id: string){
 		// let token = this.authService.getToken();
-		const url_api=`https://db.bbevolutionbank.com:3025/api/infos/${id}`;
+		const url_api=`https://db.buckapi.us:9999/api/infos/${id}`;
 		return this.http
 		.put<InfoInterface>(url_api, info)
 		.pipe(map(data => data));
 	}
 	saveTransaction(transaction :TransactionInterface){
-		const url_api='https://db.bbevolutionbank.com:3025/api/transaction';
+		const url_api='https://db.buckapi.us:9999/api/transaction';
 		return this.http
 		.post<TransactionInterface>(url_api, transaction)
 		.pipe(map(data => data));
 	}
 	getTransactionsByUserId(userId: string){
 		let indice = userId;
-		const url_api =  "https://db.bbevolutionbank.com:3025/api/transaction?filter[where][userId]=p"+indice;
+		const url_api =  "https://db.buckapi.us:9999/api/transaction?filter[where][userId]=p"+indice;
 		this.transaction = this.http.get(url_api);
 		return (this.transaction);
 	}
 	getCreditcardsByUserId(userId: string){
 		let indice = userId;
-		const url_api =  "https://db.bbevolutionbank.com:3025/api/creditcard?filter[where][userId]=p"+indice;
+		const url_api =  "https://db.buckapi.us:9999/api/creditcard?filter[where][userId]=p"+indice;
 		this.creditcard = this.http.get(url_api);
 		return (this.creditcard);
 	}
 	getAccountByEmail(userEmail: string){
 		let email = userEmail;
-		const url_api =  "https://db.bbevolutionbank.com:3025/api/account?filter[where][email]="+email;
+		const url_api =  "https://db.buckapi.us:9999/api/account?filter[where][email]="+email;
 		this.account = this.http.get(url_api);
 		return (this.account);
 	}
